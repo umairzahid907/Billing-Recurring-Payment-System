a969478143764a5654b2c29d4a8c4d182db0f527
Muhammad Umair Zahid <umair.zahid@devsinc.com>
2022-09-25 12:20:56 +0500

    checking:

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   include Pundit::Authorization
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   private
