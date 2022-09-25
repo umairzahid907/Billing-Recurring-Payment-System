fa1b3416b0f69027e402edd1fd87bfc0d63b7780
Muhammad Umair Zahid <umair.zahid@devsinc.com>
2022-09-25 06:49:51 +0500

    addded url in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'https://billing-app-umair.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
